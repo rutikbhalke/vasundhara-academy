3af2093e9c0903c0fa062d7253048fc3317a7791
Rutik Bhalke <rutikbhalke20@gmail.com>
2026-06-19 13:23:53 +0530

    Fix activity auth options import

--- src/lib/activity.js
+++ src/lib/activity.js
@@ -1,6 +1,6 @@
 import prisma from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/authOptions';
 
 /**
  * Log an admin action to the activity log
